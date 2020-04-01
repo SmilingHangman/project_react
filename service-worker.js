420881888850b6fb1f9e18d69ef9e78989d2c3ab
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-04-01 23:22:52 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/SmilingHangman/project_react/precache-manifest.7b331fb8d5fe696197c4608869517525.js"
+  "/SmilingHangman/react_project_build_test/precache-manifest.9a0ca8a67548cc34ecba6f4c9e78b693.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SmilingHangman/project_react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/SmilingHangman/react_project_build_test/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
