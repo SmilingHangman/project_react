a64112ee8773a860bc67d52cae53a1d56e4fe225
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-04-02 00:04:54 +0300

    Updates

---
+++ precache-manifest.cac3422a81544192ddfdd6842b7c555f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "376d7fde9fa8640a6ba2766401f98f4e",
+    "url": "/react_project_build_test/index.html"
+  },
+  {
+    "revision": "fcbd1c972e924c7d80a2",
+    "url": "/react_project_build_test/static/css/main.aebb10ee.chunk.css"
+  },
+  {
+    "revision": "29a95fd054d545dfb110",
+    "url": "/react_project_build_test/static/js/2.fc890b90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react_project_build_test/static/js/2.fc890b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbd1c972e924c7d80a2",
+    "url": "/react_project_build_test/static/js/main.fd223b5d.chunk.js"
+  },
+  {
+    "revision": "68924f65ffc1ed46f641",
+    "url": "/react_project_build_test/static/js/runtime-main.679c07d4.js"
+  },
+  {
+    "revision": "d14b8d58f16da0ad8e16725632c9bb94",
+    "url": "/react_project_build_test/static/media/beer-20s-1084px.d14b8d58.svg"
+  },
+  {
+    "revision": "0848875155aec7e1423cd218a28fbff0",
+    "url": "/react_project_build_test/static/media/belch.08488751.wav"
+  },
+  {
+    "revision": "92c3baceb9b5f9223306366674dd43e8",
+    "url": "/react_project_build_test/static/media/dun_dun_dun.92c3bace.wav"
+  },
+  {
+    "revision": "b3e67645bda7f450140c32771503374f",
+    "url": "/react_project_build_test/static/media/dustin-belt-DCQilQQ_p7A-unsplash.b3e67645.jpg"
+  },
+  {
+    "revision": "26ea04917eb8780a4f68bc4b6b5e5aba",
+    "url": "/react_project_build_test/static/media/mute_sound.26ea0491.svg"
+  },
+  {
+    "revision": "eabd1212e51dfcfb1196a74f4c5b5ea4",
+    "url": "/react_project_build_test/static/media/play_sound.eabd1212.svg"
+  },
+  {
+    "revision": "76d325c08570c53dd55e694a41580408",
+    "url": "/react_project_build_test/static/media/pub_ambience.76d325c0.mp3"
+  },
+  {
+    "revision": "6c0edf1222981871c73472a25ba14ce4",
+    "url": "/react_project_build_test/static/media/swallow.6c0edf12.mp3"
+  },
+  {
+    "revision": "09fb21c5db3b22f35f81d92526bb2d19",
+    "url": "/react_project_build_test/static/media/woah.09fb21c5.wav"
+  }
+]);
