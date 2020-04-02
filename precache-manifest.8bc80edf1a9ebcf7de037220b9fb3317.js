ab4c5a28214179c20375e98e569993de5baa7f4d
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-04-02 21:36:18 +0300

    Updates

---
+++ precache-manifest.8bc80edf1a9ebcf7de037220b9fb3317.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28bca40557aa4571bc5ee26a6b4a5055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ca0232ee3690136f09",
+    "url": "/static/css/main.01ab40dd.chunk.css"
+  },
+  {
+    "revision": "29a95fd054d545dfb110",
+    "url": "/static/js/2.fc890b90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.fc890b90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1ca0232ee3690136f09",
+    "url": "/static/js/main.d2bcee0c.chunk.js"
+  },
+  {
+    "revision": "747976be659fb8962102",
+    "url": "/static/js/runtime-main.da6e50b7.js"
+  },
+  {
+    "revision": "d14b8d58f16da0ad8e16725632c9bb94",
+    "url": "/static/media/beer-20s-1084px.d14b8d58.svg"
+  },
+  {
+    "revision": "0848875155aec7e1423cd218a28fbff0",
+    "url": "/static/media/belch.08488751.wav"
+  },
+  {
+    "revision": "92c3baceb9b5f9223306366674dd43e8",
+    "url": "/static/media/dun_dun_dun.92c3bace.wav"
+  },
+  {
+    "revision": "b3e67645bda7f450140c32771503374f",
+    "url": "/static/media/dustin-belt-DCQilQQ_p7A-unsplash.b3e67645.jpg"
+  },
+  {
+    "revision": "26ea04917eb8780a4f68bc4b6b5e5aba",
+    "url": "/static/media/mute_sound.26ea0491.svg"
+  },
+  {
+    "revision": "eabd1212e51dfcfb1196a74f4c5b5ea4",
+    "url": "/static/media/play_sound.eabd1212.svg"
+  },
+  {
+    "revision": "76d325c08570c53dd55e694a41580408",
+    "url": "/static/media/pub_ambience.76d325c0.mp3"
+  },
+  {
+    "revision": "6c0edf1222981871c73472a25ba14ce4",
+    "url": "/static/media/swallow.6c0edf12.mp3"
+  },
+  {
+    "revision": "09fb21c5db3b22f35f81d92526bb2d19",
+    "url": "/static/media/woah.09fb21c5.wav"
+  }
+]);
