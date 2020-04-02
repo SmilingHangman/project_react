f8ca52d46675802afa24442bbb3ea65ceb58484c
SmilingHangman <andrejus.chomicenka@gmail.com>
2020-04-02 22:50:58 +0300

    some weird workaround to sart-up build version

--- src/pages/404/404.js
+++ src/pages/404/404.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const Error = () => {
   return (
     <div>
       <h2>404! This page does not exist!</h2>
+      <Link to='/'>Link</Link>
     </div>
   )
 }
